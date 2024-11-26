c41e58e2e60c0af202b8aff219c1373dc370c12b
Alonso Bravo <alsonsobravo139@gmail.com>
2024-11-26 14:37:23 -0800

    Update

--- static/app.js
+++ static/app.js
@@ -1,63 +1,55 @@
 const socket = io.connect('http://172.16.129.19:5000');
 
-//prompts user for a username
+// Prompts user for a username
 let username = prompt("Enter Username:");
 
-//notifies server of new connection
-socket.emit('new_connection', {username:username});
+// Notifies server of new connection
+socket.emit('new_connection', { username: username });
 
-//function to send messages
+// Function to send messages
 function sendMessage() {
     const inputElement = document.getElementById('message-input');
     const message = inputElement.value;
 
     if (message) {
-        socket.emit('send_message', { message: message, username:username });
+        socket.emit('send_message', { message: message, username: username });
         inputElement.value = '';  // Clear the input field
     }
 }
 
-//handle 'user_connected' events
-socket.on('user_connected', function(data){
+// Handle 'user_connected' events
+socket.on('user_connected', function(data) {
     const chatBox = document.getElementById('chat-box');
     const userElement = document.createElement('p');
 
     userElement.innerText = `${data.username} has entered the chat.`;
-
     chatBox.appendChild(userElement);
 });
 
-//listen for messages from the server
+// Listen for messages from the server
 socket.on('receive_message', function(data) {
     const chatBox = document.getElementById('chat-box');
     const messageElement = document.createElement('p');
 
     messageElement.innerText = `${data.username}: ${data.message}`;
-
-    // Append the message to the chat box
     chatBox.appendChild(messageElement);
 });
 
-//username taken event
-socket.on('username_taken', function(data){
+// Handle 'username_taken' event
+socket.on('username_taken', function(data) {
     alert(data.error);
     username = prompt("Username Taken, try again");
-    socket.emit('new_connection',{username:username});
+    socket.emit('new_connection', { username: username });
 });
 
-//handle 'update_user_list'
-socket.on('update_user_list', function(users){
+// Handle 'update_user_list'
+socket.on('update_user_list', function(users) {
     const userList = document.getElementById('user-list');
     userList.innerHTML = '<h2>Users</h2>';
 
-    users.forEach(function(user){
+    users.forEach(function(user) {
         const userElement = document.createElement('p');
         userElement.innerText = user;
         userList.appendChild(userElement);
     });
 });
-
-
-
-
-
